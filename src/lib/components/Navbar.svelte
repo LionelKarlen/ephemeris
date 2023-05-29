a3c026a32be4b02842dd462afee5666aea74d3d9
LionelKarlen <lionel.karlen@protonmail.com>
2023-05-29 15:59:54 +0200

    feat: Add tailwind and daisyui

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const ROUTES = [
+		{
+			name: 'Sign out',
+			route: '/signout'
+		},
+		{
+			name: 'archive',
+			route: '/archive'
+		},
+		{
+			name: 'next year',
+			route: '/next'
+		}
+	];
+</script>
