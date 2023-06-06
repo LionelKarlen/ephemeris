6076d0d0ae150ec2763f95b6683f6dcaca2e1b01
LionelKarlen <lionel.karlen@protonmail.com>
2023-06-06 13:36:46 +0200

    feat: Add next year

---
+++ src/routes/next/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import MonthTable from '$lib/components/MonthTable.svelte';
+	import { DateTime } from 'luxon';
+	let dateTime = DateTime.utc();
+</script>
+
+<MonthTable month={dateTime.month} year={dateTime.year + 1} />
